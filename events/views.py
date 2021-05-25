575d89340e265324d6304df25ecfdad344142455
Sergei Vorobev <vorobevse86@gmail.com>
2021-05-25 10:54:59 +0200

    25MayCommit

--- events/views.py
+++ events/views.py
@@ -5,10 +5,13 @@ from datetime import datetime
 from .models import Event, Venue
 from .forms import VenueForm, EventForm, CreateUser
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     # name = "John"
     month = month.capitalize()
@@ -24,44 +27,44 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
     # Get current time
     time = now.strftime('%I:%M:%S %p')
     return render(request, 'events/home.html', {
-        # "name": name,
-        "year": year,
-        "month": month,
-        "month_number": month_number,
-        "cal": cal,
-        "current_year": current_year,
-        "time": time,
-    })
+                      # "name": name,
+                      "year": year,
+                      "month": month,
+                      "month_number": month_number,
+                      "cal": cal,
+                      "current_year": current_year,
+                      "time": time,
+                  })
 
-
+@login_required(login_url='login')
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'events/list_event.html', {
-        "event_list": event_list,
-    })
+                      "event_list": event_list,
+                  })
 
-
+@login_required(login_url='login')
 def all_venues(request):
     venues_list = Venue.objects.all()
     return render(request, 'events/list_venue.html', {
-        "venue_list": venues_list,
-    })
+                      "venue_list": venues_list,
+                  })
 
-
+@login_required(login_url='login')
 def show_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     return render(request, 'events/venue_show.html', {
-        "venue": venue,
-    })
+                      "venue": venue,
+                  })
 
-
+@login_required(login_url='login')
 def show_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     return render(request, 'events/event_show.html', {
-        "event": event,
-    })
+                      "event": event,
+                  })
 
-
+@login_required(login_url='login')
 def add_venue(request):
     submitted = False
     if request.method == "POST":
@@ -76,7 +79,7 @@ def add_venue(request):
 
     return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted})
 
-
+@login_required(login_url='login')
 def add_event(request):
     submitted = False
     if request.method == "POST":
@@ -91,25 +94,25 @@ def add_event(request):
 
     return render(request, 'events/add_event.html', {'form': form, 'submitted': submitted})
 
-
+@login_required(login_url='login')
 def search_venues(request):
     if request.method == 'POST':
         searched = request.POST['searched']
         venues = Venue.objects.filter(name__contains=searched)
-        return render(request, 'events/venue_search.html', {'searched': searched, 'venues': venues})
+        return render(request, 'events/search_venues.html', {'searched': searched, 'venues': venues})
     else:
-        return render(request, 'events/venue_search.html', {})
+        return render(request, 'events/search_venues.html', {})
 
-
+@login_required(login_url='login')
 def search_events(request):
     if request.method == 'POST':
         searched = request.POST['searched']
         events = Event.objects.filter(name__contains=searched)
-        return render(request, 'events/event_search.html', {'searched': searched, 'events': events})
+        return render(request, 'events/search_events.html', {'searched': searched, 'events': events})
     else:
-        return render(request, 'events/event_search.html', {})
+        return render(request, 'events/search_events.html', {})
 
-
+@login_required(login_url='login')
 def edit_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     form = VenueForm(request.POST or None, instance=venue)
@@ -117,9 +120,9 @@ def edit_venue(request, venue_id):
         form.save()
         return redirect('show-venue', venue_id)
 
-    return render(request, 'events/edit_venue.html', {'venue': venue, 'form': form})
+    return render(request, 'events/edit_venue.html', {'venue': venue,'form': form})
 
-
+@login_required(login_url='login')
 def edit_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     form = EventForm(request.POST or None, instance=event)
@@ -127,33 +130,56 @@ def edit_event(request, event_id):
         form.save()
         return redirect('show-event', event_id)
 
-    return render(request, 'events/edit_event.html', {'event': event, 'form': form})
+    return render(request, 'events/edit_event.html', {'event': event,'form': form})
 
-
+@login_required(login_url='login')
 def del_event(request, event_id):
     event = Event.objects.get(pk=event_id)
     event.delete()
     return redirect('list-events')
 
-
+@login_required(login_url='login')
 def del_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     venue.delete()
     return redirect('list-venues')
 
-
 def registration(request):
-    form = CreateUser()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUser()
 
-    if request.method == 'POST':
-        form = CreateUser(request.POST)
-        if form.is_valid():
-            form.save()
+        if request.method=='POST':
+            form = CreateUser(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
 
-    context = {'form': form}
-    return render(request, 'events/registration.html', context)
+        context = {'form': form}
+        return render(request, 'events/registration.html', context)
 
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, password=password, username=username)
 
-def login(request):
-    context = {}
-    return render(request, 'events/login.html', context)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is not correct')
+
+        context = {}
+        return render(request, 'events/login.html', context)
+
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
